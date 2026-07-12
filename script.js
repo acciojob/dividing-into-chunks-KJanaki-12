c8252b888cbcd42f7adb5f4b8396956d1a7218bf
KJanaki-12 <kenchugundujanaki@gmail.com>
2026-07-12 19:23:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+const arr = [4, 3, 2, 1];
 
 const divide = (arr, n) => {
   // Write your code here
+	let ans = [];
+	let sum = 0;
+	let prefixSum = 0;
+	let j = 0;
+	let k = 0;
+	for(let i=0; i<arr.length; i++){
+		prefixSum += arr[i];
+		if(prefixSum > n){
+			ans[k++] = arr.slice(j, i);
+			j = i;
+			i = i-1;
+			prefixSum = 0;
+		}
+	}
+	ans[k] = arr.slice(j, arr.length);
+	return ans;
 };
 
 const n = prompt("Enter n: ");
